cf3c423f1a164c7e98491db81c83c8da7bfd9978
Asia Kane <asialkane@gmail.com>
2017-04-28 09:35:36 -0700

    add passing unit tests and methods to assign a unique id to each word and find a word by its id

--- lib/word.rb
+++ lib/word.rb
@@ -1,10 +1,11 @@
 class Word
-  attr_reader(:name, :type)
   @@words = []
+  attr_reader(:name, :type, :id)
 
   def initialize (attributes)
     @name = attributes.fetch(:name)
     @type = attributes.fetch(:type)
+    @id = @@words.length + 1
   end
 
   def self.all
@@ -18,5 +19,16 @@ class Word
   def self.clear
     @@words = []
   end
+
+  def self.find(unique_id)
+    matched_word = nil
+    @@words.each do |word|
+      if word.id == unique_id
+        matched_word = word
+      end
+    end
+    matched_word
+  end
+
 
 end #class Word end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,6 +4,10 @@ require 'pry'
 require 'definition'
 
 describe(Word) do
+  before() do
+    Word.clear
+  end
+
   describe('#name') do
     it('returns the name of the word') do
       test_word = Word.new({:name => 'discover', :type => 'verb'})
@@ -36,6 +40,23 @@ describe(Word) do
         test_word = Word.new({:name => 'discover', :type => 'verb'}).save
         Word.clear
         expect(Word.all).to(eq([]))
+      end
+    end
+
+    describe('#id') do
+      it('returns the id of the word') do
+        test_word = Word.new({:name => 'discover', :type => 'verb'})
+        expect(test_word.id).to(eq(1))
+      end
+    end
+
+    describe('.find') do
+      it('finds a word by the id and returns the word') do
+        test_word = Word.new({:name => 'discover', :type => 'verb'})
+        test_word.save
+        test_word2 = Word.new({:name => 'ambition', :type => 'noun'})
+        test_word2.save
+        expect(Word.find(test_word.id)).to(eq(test_word))
       end
     end
 end #Class Word end
