851b54ec4932c5f2c2b71caef1953ca361ebf40e
Asia Kane <asialkane@gmail.com>
2017-04-28 11:47:39 -0700

    add user integration test to visit the homepage, click on a word that has already been added to the dictionary, be taken to that word page, click add definition link, be taken to the definition form, click add definition button, be taken to the success page, click back to word button, and be taken back word page

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -5,17 +5,33 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the word path', {:type => :feature}) do
-  it('visits the homepage and clicks Add New Word button') do
+  it('visits the homepage, clicks Add New Word button, and is taken to the word form') do
     visit('/')
     click_link('Add a word')
     expect(page).to have_content('Add a Word to Your Dictionary:')
   end
 
-  it('visits the add word form and fills in form, then clicks Add Your Word button') do
+  it('visits the add word form and fills in form, clicks Add Your Word button, is redirected to success page, clicks back to homepage, and is taken back to homepage') do
     visit('/words/new')
     fill_in('name', :with => 'discover')
     fill_in('type', :with => 'verb')
     click_button('Add Your Word')
     expect(page).to have_content("You've successfully added a word!")
+    click_link('Back to homepage')
+    expect(page).to have_content('Create a personalized dictionary with words you would like to learn!')
   end
+
+  it('visits the homepage, clicks on a word that has already been added to the dictionary, is taken to that words page, clicks add definition link, is taken to the definition form, clicks add definition button, is taken to the success page') do
+    visit('/')
+    click_link('discover (verb)')
+    expect(page).to have_content('Definition(s):')
+    click_link('Add a definition')
+    expect(page).to have_content('Add a definition to "discover" :')
+    fill_in('text', :with => 'find unexpectedly or in the course of a search')
+    click_button('Add Definition')
+    expect(page).to have_content("You've successfully added a definition!")
+    click_link('Back to word')
+    expect(page).to have_content('Definition(s): find unexpectedly or in the course of a search')
+  end
+
 end
