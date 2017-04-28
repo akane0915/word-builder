8420649b7b64d6691ac62d5fb5afc9236c148286
Asia Kane <asialkane@gmail.com>
2017-04-28 09:10:14 -0700

    add passing unit tests and methods to return type of word, save word, return words array, and clear words array

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,22 @@
 class Word
   attr_reader(:name, :type)
-  
+  @@words = []
+
   def initialize (attributes)
     @name = attributes.fetch(:name)
     @type = attributes.fetch(:type)
+  end
+
+  def self.all
+    @@words
+  end
+
+  def save
+    @@words.push(self)
+  end
+
+  def self.clear
+    @@words = []
   end
 
 end #class Word end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,4 +10,32 @@ describe(Word) do
       expect(test_word.name).to(eq('discover'))
     end
   end
-end
+
+  describe('#type') do
+    it('returns the type of the word') do
+      test_word = Word.new({:name => 'discover', :type => 'verb'})
+      expect(test_word.type).to(eq('verb'))
+    end
+  end
+
+  describe('.all') do
+    it('returns the array of all words; it is empty to start') do
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a word to the words array') do
+      test_word = Word.new({:name => 'discover', :type => 'verb'}).save
+      expect(Word.all).to(eq(test_word))
+    end
+  end
+
+    describe('.clear') do
+      it('clears the words array') do
+        test_word = Word.new({:name => 'discover', :type => 'verb'}).save
+        Word.clear
+        expect(Word.all).to(eq([]))
+      end
+    end
+end #Class Word end
